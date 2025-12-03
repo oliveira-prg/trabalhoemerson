a5b8bfae9ed276b4b6e66365ff8f899e0f7137da
Gabriel Colares <gabriel.colares.dev@gmail.com>
2025-12-03 19:37:45 -0400

    docs: make changes

--- trabalho-gabriel/regras_inferencia.py
+++
@@ -1,5 +0,0 @@
-def particularizacao_universal(expressao, constante):
-    return expressao.replace("x", constante)
-
-def generalizacao_existencial(expressao, variavel):
-    return f"(∃{variavel}){expressao}"
